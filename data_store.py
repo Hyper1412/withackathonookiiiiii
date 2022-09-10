36dc62610bd87146ef173218668cca2a5c218800
Christine Phung <z5363653@ad.unsw.edu.au>
2022-09-10 23:17:50 +1000

    set_limit function

---
+++ data_store.py
@@ -0,0 +1,25 @@
+initial_object = {
+    "users": [{
+        "id": 1,
+        "name": "chris",
+        "individual_limit": 0,
+    }]
+}
+
+
+class Datastore:
+    def __init__(self):
+        self.__store = initial_object
+
+    def get(self):
+        return self.__store
+
+    def set(self, store):
+        if not isinstance(store, dict):
+            raise TypeError('store must be of type dictionary')
+        self.__store = store
+
+print('Loading Datastore...')
+
+global data_store
+data_store = Datastore()
--- set_limit.py
+++ set_limit.py
@@ -1,3 +1,8 @@
-def set_limit():
-    return {}
-    
+import data_store
+
+store = data_store.get()
+
+def set_limit(limit):
+    user = store["users"][0]
+    user["individual_limit"] = 10
+    return {user["individual_limit"]}
